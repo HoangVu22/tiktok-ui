9bbcd63cf3478a203a12c5e51ff4815428503ebc
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2023-06-04 15:19:03 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -1,5 +1,8 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+
+console.log(images.logo)
 
 const cx = classNames.bind(styles)
 
@@ -7,7 +10,23 @@ function Header() {
     return (  
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+
+                <div className={cx('search')}>
+                    <input type="text" placeholder='Search accounts and videos' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <i className="bi bi-x-circle-fill"></i>
+                    </button>
+                    {/* <i className={`bi bi-arrow-repeat ${cx('loading')}`}></i> */}
+
+                    <button className={cx('search-btn')}>
+                        <i className="bi bi-search"></i>
+                    </button>
+                </div>
+
+                <div className={cx('action')}></div>
             </div>
         </header>
     );
